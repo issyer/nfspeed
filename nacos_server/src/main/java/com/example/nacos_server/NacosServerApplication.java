f0410fed77dcd2d254d8786e697ce5f177da4217
sunwanghe <swh@sengled.com>
2021-10-06 21:54:45 +0800

    提交

--- nacos_server/src/main/java/com/example/nacos_server/NacosServerApplication.java
+++ nacos_server/src/main/java/com/example/nacos_server/NacosServerApplication.java
@@ -2,12 +2,17 @@ package com.example.nacos_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.nacos_server")
+@ServletComponentScan
+@EnableScheduling
 public class NacosServerApplication {
 
     public static void main(String[] args) {
-        //test
+        System.setProperty("nacos.standalone", "true");
+
         SpringApplication.run(NacosServerApplication.class, args);
     }
 

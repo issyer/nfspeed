e4c1296b86529f03e67adc644085a3cbd386dfdf
sunwanghe <swh@sengled.com>
2021-10-06 14:51:19 +0800

    Initial commit

---
+++ nacos_server/src/main/java/com/example/nacos_server/NacosServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacos_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class, args);
+    }
+
+}
